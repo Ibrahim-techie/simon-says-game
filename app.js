c2a849501bbbfdf3c0bad4e0873aed05739d2205
Ibrahim-techie <ibrahimhatodwala@gmail.com>
2026-06-04 17:55:31 +0530

    Finish UI interaction Part

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
 let h2= document.querySelector('h2');
 
-let btns=document.querySelectorAll('btn');
+
 let randombtns=["red","yellow","green","purple"];
 let startgame=false;
-document.addEventListener('keypress',function(){
+document.addEventListener('keydown',function(){
     if(!startgame){
     h2.innerText="Level1";
     startgame=true;
+    levelUp();
     }
 
-    levelUp();
+    
 });
 
 let level=0;
 function levelUp(){
 level++;
 
-btnflash();
+
+machineflash();
 }
 
-function btnflash(){//machineflash
+function machineflash(){//machineflash
     let randomcolridx = Math.floor(Math.random() * randombtns.length);
-    let colorval=randombtns[`${randomcolridx}`];
+   let colorval = randombtns[randomcolridx];
  
     let btn=document.querySelector(`.${colorval}`);
    
-    btn.classList.add('flash');
+    btn.classList.add('machineflash');
 
 setTimeout(function() {
-    btn.classList.remove('flash');
+    btn.classList.remove('machineflash');
+}, 250);
+}
+
+
+function userflash(btn){
+
+   
+    btn.classList.add('userflash');
+
+setTimeout(function() {
+    btn.classList.remove('userflash');
 }, 250);
 }
 
 
 
+let clickbtn=document.querySelectorAll('.btn');
+
+for(let btn of clickbtn){
+    btn.addEventListener('click', function(){
+        userflash(this);
+    });
+}
